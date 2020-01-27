b7025b458fb312147419538b0472c185379b5920
Ying Quan Tan <ying.quan.tan@gmail.com>
2020-01-26 21:16:06 -0500

    update required courses with setup information

---
+++ lambda-app/scripts/check_venv.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def is_venv():
+    return hasattr(sys, 'real_prefix') or \
+            (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix)
+
+
+if is_venv():
+    pass
+else:
+    sys.exit(1)
